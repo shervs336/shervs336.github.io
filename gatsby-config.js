8329d4661b28f1ee747415208cb03ed940e27eac
Sherwin Rhey Condez <sherwinrheycondez@gmail.com>
2021-10-12 15:11:35 +0800

    Updates

--- gatsby-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  pathPrefix: "/",
-  siteMetadata: {
-    siteUrl: "shervs336.github.io",
-    title: "Sherwin Rhey Condez Official Website",
-  },
-  plugins: ["gatsby-plugin-postcss"],
-};
